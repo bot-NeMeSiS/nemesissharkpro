12b206fc731e2429f6e05c6b9e0935cdd317a378
bot-NeMeSiS <damianalonsoruizdayda@gmail.com>
2026-05-16 02:07:49 +0200

    yy

---
+++ README_V324.md
@@ -0,0 +1,9 @@
+# V324 · SMART CACHE + LIVE PERFORMANCE PRO
+
+Incluye:
+- smart_cache_engine.py
+- snapshot_cache.py
+- timeline_cache.py
+- cache de snapshots
+- expiración inteligente
+- reutilización de timelines
---
+++ cache_layer/smart_cache_engine.py
@@ -0,0 +1,9 @@
+class SmartCacheEngine:
+    def __init__(self):
+        self.cache = {}
+
+    def get(self, key):
+        return self.cache.get(key)
+
+    def set(self, key, value):
+        self.cache[key] = value
